06add4266114d9aa5ef51abba4b1ee2a3587da56
Jianping145 <jianpingwu145@gmail.com>
2026-09-12 13:34:46 +0800

    Add files via upload

--- beeg.js
+++ beeg.js
@@ -1,4 +1,3 @@
-//来自“John Smith”
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
 const API_BASE = 'https://store.externulls.com'
